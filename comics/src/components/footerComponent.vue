ac81fe5dc52ba6adef2b48b7750c064d236a81d2
giadavolpin <volpingiada@gmail.com>
2022-11-13 17:18:39 +0100

    main fatto

--- comics/src/components/footerComponent.vue
+++ comics/src/components/footerComponent.vue
@@ -1,20 +1,25 @@
 <template>
     <footer>
-        <div class="bordoazzur uppercase">
-            sing-up now!
+        <div class="backg-comic">
+
         </div>
-        <div class="uppercase azzur">follow us</div>
-        <ul>
-            <li>
-                <link rel="stylesheet" href="comics\vue-dc-comics-1\img\footer-facebook.png">
-            </li>
-            <li>
-                <link rel="stylesheet" href="comics\vue-dc-comics-1\img\footer-twitter.png">
-            </li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
+        <div class="">
+            <div class="bordoazzur uppercase">
+                sing-up now!
+            </div>
+            <div class="uppercase azzur">follow us</div>
+            <ul>
+                <li>
+                    <link rel="stylesheet" href="comics\vue-dc-comics-1\img\footer-facebook.png">
+                </li>
+                <li>
+                    <link rel="stylesheet" href="comics\vue-dc-comics-1\img\footer-twitter.png">
+                </li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
     </footer>
 
 
@@ -27,9 +32,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-footer {
-    background-color: rgb(48, 48, 48);
+.backg-comic {
+    background-image: url(../assets/vue-dc-comics-1/img/footer-bg.jpg);
+    height: 270px;
 }
+
+/*     {
+    background-color: rgb(48, 48, 48);
+} */
 
 .uppercase {
     text-transform: uppercase;
--- comics/src/components/mainComponent.vue
+++ comics/src/components/mainComponent.vue
@@ -1,10 +1,23 @@
 <template>
-    <div class="nero">
-        --> Content goes here </div>
+    <p class="nero">
+        --> Content goes here </p>
 
-    <div>
+    <div class="azzurro-sfondo uppercase d-flex justify-content-center flex-row align-items-center">
+
+        <img class=" mis-azz m-1" src="../assets/vue-dc-comics-1/img/buy-comics-digital-comics.png" alt="">
+        <p class=" uppercase m-2">digital comics</p>
+        <img class=" mis-azz m-1" src="../assets/vue-dc-comics-1/img/buy-comics-merchandise.png" alt="">
+        <p class=" uppercase m-2">dc merchandise</p>
+        <img class=" mis-azz m-1" src="../assets/vue-dc-comics-1/img/buy-comics-subscriptions.png" alt="">
+        <p class=" uppercase m-2">subscription</p>
+        <img class=" mis-azz m-1" src="../assets/vue-dc-comics-1/img/buy-comics-shop-locator.png" alt="">
+        <p class=" uppercase m-2">comic shop locator</p>
+        <img class=" mis-azz m-1" src="../assets/vue-dc-comics-1/img/buy-dc-power-visa.svg" alt="">
+        <p class=" uppercase m-2">dc power visa</p>
 
     </div>
+
+
 </template>
 
 <script>
@@ -18,5 +31,22 @@ export default {
     background-color: black;
     height: 70px;
     color: white;
+    margin-bottom: 0;
+}
+
+.azzurro-sfondo {
+    background-color: rgb(2, 130, 249);
+    color: white;
+    height: 100px;
+}
+
+.uppercase {
+    text-transform: uppercase;
+    font-size: 12px;
+}
+
+.mis-azz {
+    width: 25px;
+    height: 30px;
 }
 </style>
